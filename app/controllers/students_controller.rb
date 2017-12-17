82fb72fd307c6f40572ba05b1f3559207ae74bd0
Flatiron School <leslie.hsu@flatironschool.com>
2017-12-16 21:13:07 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,14 @@
 class StudentsController < ApplicationController
+
   def index
     @students = Student.all
+    #implicitly renders views/students/index
   end
+
+  def show
+    @student = Student.find(params[:id])
+    #implicitly renders views/students/show
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :students, only: :index
+
+  #below added by me - to draw in the show route that looks for students#show method; looks for the students/show.html
+  resources :students, only: :show
+
 end
